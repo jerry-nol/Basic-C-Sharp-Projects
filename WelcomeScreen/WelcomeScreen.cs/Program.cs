9675689cfbaad769b140d99a6a4c5e00722aa9e5
jerry-nol <newyork_14@live.com>
2021-05-03 19:46:36 -0600

    Welcome Screen Assignment

---
+++ WelcomeScreen/WelcomeScreen.cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Welcome to Acme Accounting System \n" +
+			"Remember we are \"acounting\" on you!");
+		Console.ReadLine();
+	}
+
+}
