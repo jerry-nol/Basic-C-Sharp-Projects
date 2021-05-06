4a8bcd97277d3dc4d0f04cb199b27d9fdfbc6661
jerry-nol <newyork_14@live.com>
2021-05-05 22:28:21 -0600

    Car insurance Program

---
+++ CarInsuranceProgram/CarInsuranceProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Program asks questions to qualify for car insurance
+            Console.WriteLine("Do you qualify for our car insurance? \nWhat is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Have you had any DUI's. Please answer \"true\" or \"false\".");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets have you had before?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            // User answers must all be true
+            Console.WriteLine(age >= 15 && tickets < 3 && dui != true);
+
+            Console.ReadLine();
+        }
+    }
+}
