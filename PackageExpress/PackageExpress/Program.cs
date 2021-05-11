062d1c780ed67f18127952a95b1726fe88986b1e
jerry-nol <newyork_14@live.com>
2021-05-10 18:31:08 -0600

    Update

--- PackageExpress/PackageExpress/Program.cs
+++ PackageExpress/PackageExpress/Program.cs
@@ -19,33 +19,34 @@ namespace PackageExpress
             if (weight <= 50)
             {
                 Console.WriteLine("What is the width?");
+
+                // Getting all the dimensions of package
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the height?");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the length");
+                int length = Convert.ToInt32(Console.ReadLine());
+
+                // All must be true to ship package
+                if (width <= 50 && height <= 50 && length <= 50)
+                {
+                    // calculating cost from dimensions
+                    int LxWxH = length * width * height;
+                    int xWeight = weight * LxWxH;
+                    decimal total = xWeight / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is $" + total + ".00");
+                }
+                else
+                {
+                    Console.WriteLine("The package is too big to be shipped via Package Express. \nAll dimensions of package must be no more than 50 inches");
+                }
+
             }
             else
             {
                 Console.WriteLine("Your package is too heavy to be shipped via Package Express.");
             }
-            
-            // Getting all the dimensions of package
-            int width = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("What is the height?");
-            int height = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("What is the length");
-            int length = Convert.ToInt32(Console.ReadLine());
-
-            // All must be true to ship package
-            if (width <= 50 && height <= 50 && length <= 50)
-            {
-                // calculating cost from dimensions
-                int LxWxH = length * width * height;
-                int xWeight = weight * LxWxH;
-                decimal total = xWeight / 100;
-                Console.WriteLine("Your estimated total for shipping this package is $" + total + ".00");
-            }
-            else
-            {
-                Console.WriteLine("The package is too big to be shipped via Package Express. \nAll dimensions of package must be no more than 50 inches");
-            }
-
+         
             // Final message
             Console.WriteLine("Thank you.");
             Console.ReadLine();
