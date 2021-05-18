40924543d249a7e0a1f77babeec904473e7298c7
jerry-nol <newyork_14@live.com>
2021-05-17 20:14:16 -0600

    Methods

---
+++ Methods/Methods/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Class1
+    {
+        public int num1 = 5;
+    }
+    public class Class2
+    {
+        public int num2 = 15;
+    }
+    public class Class3
+    {
+        public int num3 = 10;
+    }
+
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //adding class1 method to the main program
+            Class1 add = new Class1();
+            Console.WriteLine("Select an integer to be added, subtracted and multiplied by different numbers.");
+            int num4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(add.num1 + num4);
+
+            Class2 subtract = new Class2();
+            Console.WriteLine(subtract.num2 - num4);
+
+            Class3 multiply = new Class3();
+            Console.WriteLine(multiply.num3 * num4);
+
+            Console.ReadLine();
+        }
+    }
+}
