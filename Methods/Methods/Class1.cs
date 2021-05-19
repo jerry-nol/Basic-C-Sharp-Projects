535ae7efad248a78bfa2d183411662f07b561e48
jerry-nol <newyork_14@live.com>
2021-05-18 20:18:10 -0600

    Added comments

--- Methods/Methods/Class1.cs
+++ Methods/Methods/Class1.cs
@@ -8,14 +8,17 @@ namespace Methods
 {
     public class Class1
     {
+        // adding two integers
         public static int Add(int num1, int num4)
         {
             return num1 + num4;
         }
+        // Subtracting two integers
         public static int Subtract(int num2, int num4)
         {
             return num2 - num4;
         }
+        // Multipying two integers
         public static int Multiply(int num3, int num4)
         {
             return num3 * num4;
--- Methods/Methods/Program.cs
+++ Methods/Methods/Program.cs
@@ -14,7 +14,8 @@ namespace Methods
             Class1 add = new Class1();
             Console.WriteLine("Select an integer to be added, subtracted and multiplied by different numbers.");
             int num4 = Convert.ToInt32(Console.ReadLine());
-
+            
+            // Printing off all functions 
             Console.WriteLine("\n"+ Class1.Add(4,num4));
             Console.WriteLine("\n" + Class1.Subtract(9,num4));
             Console.WriteLine("\n" + Class1.Multiply(50,num4));
