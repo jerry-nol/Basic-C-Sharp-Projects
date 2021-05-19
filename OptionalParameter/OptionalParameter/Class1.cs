322fb4232cb37c5c292e69b439eabfd3d8427acd
jerry-nol <newyork_14@live.com>
2021-05-18 21:44:56 -0600

    Optional Parameter

---
+++ OptionalParameter/OptionalParameter/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class Op
+{
+    public static int Parameter(int j, [ Optional ] int n)
+    {
+        return j + n;
+    }
+}
---
+++ OptionalParameter/OptionalParameter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace OptionalParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Op op = new Op();
+            int og = Op.Parameter(5,9);
+            Console.WriteLine("Your number added ="+ og);           
+            
+            Console.ReadLine();
+        }
+    }
+}
