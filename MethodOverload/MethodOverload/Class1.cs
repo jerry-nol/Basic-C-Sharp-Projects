7214eb2676f36ab1719a0ed46394afbdc34ecb75
jerry-nol <newyork_14@live.com>
2021-05-18 19:03:54 -0600

    Method Overload

---
+++ MethodOverload/MethodOverload/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Operations
+{
+    //method 1 multiplying to integers
+    public static int Overload(int z, int r)
+    {
+        return z * r;
+    }
+    // method 2 adding two decimal numbers.
+    public static double Overload(double n, double d)
+    {
+        return n + d;
+    }
+    // method 3 adding two strings for an integer outcome 
+    //public static string Overload(string m, string f)
+    //{
+    //    m = "100";
+    //    int u = int.Parse(m);
+    //    f = "50";
+    //    int h = int.Parse(f);
+    //    return m + f;
+    //}
+}
---
+++ MethodOverload/MethodOverload/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating object
+            Operations ops = new Operations();
+            ops = Overload(3, 5);
+            
+            Console.WriteLine(ops);
+            Console.ReadLine();
+        }
+    }
+}
