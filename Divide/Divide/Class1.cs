23f25b73a733acf995894e0c3560393d188150bc
jerry-nol <newyork_14@live.com>
2021-05-22 12:06:55 -0600

    Out Parameter

---
+++ Divide/Divide/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divide
+{
+    class Divide
+    {
+        public void ByTwo(int any)
+        {
+            Console.WriteLine(any);
+        }
+        public static void ByTwo(out int L)
+        {
+            // Assigning the value to integer L
+            // Then divided by the number 2
+            L = 30;
+            L /= 2;
+        }
+    }
+}
---
+++ Divide/Divide/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Divide divide = new Divide();
+            Console.WriteLine("Pick a number to get divided by 2");
+            int any = Convert.ToInt32(Console.ReadLine());
+            int result = any / 2;
+            Console.WriteLine("\nThe number " + any + " divided by 2 = " + result);
+            
+            // Declaring variable without setting it a value
+            int O;
+            // Passing variable O int passed in with "out" keyword
+            Divide.ByTwo(out O);
+            Console.WriteLine("\nInteger L divided by 2 = " + O);
+
+            Console.ReadLine();
+        }
+    }
+}
