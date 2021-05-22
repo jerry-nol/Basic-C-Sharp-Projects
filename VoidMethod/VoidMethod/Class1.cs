59800c495b199f71860453dc81155642ce5f997a
jerry-nol <newyork_14@live.com>
2021-05-21 18:31:20 -0600

    Void Method

---
+++ VoidMethod/VoidMethod/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Class1
+    {
+        public void MathOp(int y, int n)
+        {
+            // Adding both parameters together
+            Console.WriteLine(y + n);
+            // Displaying just the second parameter
+            Console.WriteLine(n);
+        }
+        // assigned parameter with name
+        public void FullName(string FirstName, string LastName)
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+            
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            // taking in both parameters from method
+            class1.MathOp(7,7);
+
+
+            
+            class1.FullName(FirstName: "Banelly", LastName: "Marquez");
+            
+            Console.ReadLine();
+        }
+    }
+}
