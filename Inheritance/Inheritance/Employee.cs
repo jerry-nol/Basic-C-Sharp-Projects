7418e3fff530ed53129a86431f2cadad60f07216
jerry-nol <newyork_14@live.com>
2021-05-22 14:51:11 -0600

    Inheritance

---
+++ Inheritance/Inheritance/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    // Inheritating Person class to Employee class 
+    class Employee : Person
+    {
+        // Giving an int ID property to this class only
+        public int ID { get; set; }
+    }
+}
---
+++ Inheritance/Inheritance/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Person
+    {
+        // Two properties of persons name
+        public string FirstName{ get; set; }
+        public string LastName { get; set; }
+
+        // When method is called it will display properties given 
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
