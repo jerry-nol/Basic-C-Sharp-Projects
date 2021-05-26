a5825ec05d24e6d6ff6132e0915b3b0e54d3f963
jerry-nol <newyork_14@live.com>
2021-05-25 20:31:51 -0600

    Generic List

---
+++ GenericTyoe/GenericTyoe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTyoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating object with string type
+            Employee<string> things = new Employee<string>();
+            things.Things.Add("Laptop");
+            things.Things.Add("Notebook");
+            things.Things.Add("Pen");
+
+
+            // Instantiating object with int type
+            Employee<int> thing = new Employee<int>();
+            thing.Things.Add(1);
+            thing.Things.Add(2);
+            thing.Things.Add(3);
+
+            Console.WriteLine(thing.Things);
+            Console.ReadLine();
+         
+        }
+    }
+}
