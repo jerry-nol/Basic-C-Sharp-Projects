6f647d6ad59284d0fa4e6e19c2d950b6580560cc
jerry-nol <newyork_14@live.com>
2021-06-01 20:29:14 -0600

    Enums

---
+++ Enums/Enums/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Days
+    {
+        public WeekDay Day { get; set; }
+    }
+    public enum WeekDay
+    {
+        // giving values to days of the week.
+        Monday = 1, // if 1 is assigned the rest should follow in order.
+        Tuesday, //2
+        Wednesday, //3
+        Thursday, //4
+        Friday,  //5
+        Saturday, //6 
+        Sunday  //7
+    }
+}
---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Will make the loop repeat itself until it is entered correctly.
+            while(true)
+            try
+            {
+                Console.WriteLine("Please enter the current day of the week.");
+                string currentday = Console.ReadLine();
+                WeekDay day;
+
+                // If user enter current day correctly, print to console
+                if (Enum.TryParse<WeekDay>(currentday, out day))
+                {
+                    Console.WriteLine("Today is " + currentday);
+                    Console.ReadLine();
+                        // Breaks out of loop once correct date is entered.
+                        break;
+                }
+                else
+                {
+                    throw new OverflowException(); // try parse failed
+                }
+            }
+            
+            catch (OverflowException)
+            {
+                // Throws this message day is not valid.
+                Console.WriteLine("Not a valid day");
+            }
+            Console.ReadLine();
+        }
+    }
+}
