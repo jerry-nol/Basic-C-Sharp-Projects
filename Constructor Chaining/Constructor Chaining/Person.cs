14108941624d6bf70f84a6001011809e0a1f28ac
jerry-nol <newyork_14@live.com>
2021-06-08 13:27:27 -0600

    Chaining constructors

---
+++ Constructor Chaining/Constructor Chaining/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Chaining
+{
+    class Person
+    {
+        //Chaining two constructors
+        // 18 will be a default age if users doesn't set one 
+        public Person(string name) : this(name, 18)
+        {
+
+        }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+
+}
---
+++ Constructor Chaining/Constructor Chaining/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Chaining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // constant variable
+            const string name = "Peter";
+            // using "var" to create a variable
+            var newPerson = new Person("Frank");
+
+            Console.WriteLine(name);
+            Console.ReadLine();
+        }
+    }
+}
