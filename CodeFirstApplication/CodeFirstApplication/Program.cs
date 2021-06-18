b54d10b240af49f88191399a0be55fae262a5af4
jerry-nol <newyork_14@live.com>
2021-06-17 19:12:17 -0600

    Code-First Assignment
    Creates a data base based on the classes you have created first in your code.

---
+++ CodeFirstApplication/CodeFirstApplication/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace CodeFirstApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+                var student = new Students() { StudentName = "Jessie" };
+
+                context.Students.Add(student);
+                context.SaveChanges();
+            }
+
+
+        }
+    }
+    public class Students
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+    }
+    
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Students> Students { get; set; }
+    }
+}
+
